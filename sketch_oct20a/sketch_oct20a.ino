94ba735293392ba6486a2a845852cb3ec089a19c
Petros Evangelakos <petrosei@auth.gr>
2017-11-03 21:49:48 +0200

    Timer created and tested

--- sketch_oct20a/sketch_oct20a.ino
+++ sketch_oct20a/sketch_oct20a.ino
@@ -1,7 +1,9 @@
 //collects data from an analog sensor
 
 int sensorpin = 0;                 // analog pin used to connect the sharp sensor
-float val = 0;                 // variable to store the values from sensor(initially zero)
+float val1 = 0;                 // variable to store the values from sensor(initially zero)
+int start = 1;                 // analog pin used to connect the sharp sensor
+float val2 = 0;                 // variable to store the values from sensor(initially zero)
 
 void setup()
 {
@@ -13,12 +15,13 @@ void loop()
 {
   float volts;
   float distance;
-  val = analogRead(sensorpin);       // reads the value of the sharp sensor
-  volts = val*5/1024;
+  val1 = analogRead(sensorpin);       // reads the value of the sharp sensor
+  volts = val1*5/1024;
   distance = 28/volts;
-  //Serial.println(distance);            // prints the value of the sensor to the serial monitor
+  val2 = analogRead(start);       // reads the value of the sharp sensor
+  Serial.println(val2);            // prints the value of the sensor to the serial monitor
 
- if (distance < 100 && distance > 40){
+ if (distance < 70 && distance > 20){
   Serial.println("Enas malakas perase."); 
   
   }
